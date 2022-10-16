5a28139597417948815c8f6c456e82934e39a480
unknown <darllanferreira@hotmail.com>
2022-10-16 07:19:44 -0300

    backendProjectSpring

---
+++ dsEstudo/Backend/src/main/java/dsEstudo/dsEstudo/DsEstudoApplication.java
@@ -0,0 +1,13 @@
+package dsEstudo.dsEstudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsEstudoApplication.class, args);
+	}
+
+}
---
+++ dsEstudo/Backend/src/test/java/dsEstudo/dsEstudo/DsEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package dsEstudo.dsEstudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
